f876a1d850fe1fb70e4bd9c41b937fe02ab5dcdc
sk sohel rahaman <sohelrahaman43@gmail.com>
2024-02-03 21:18:58 +0530

    updated files after adding register component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import RegistrationForm from './components/RegistrationForm/RegistrationForm'; 
+import LoginForm from './components/Login/LoginForm';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
         <Routes>
           {/* <Route path="/" exact component={HomePage} /> */}
           <Route path="/register" element={<RegistrationForm/>} />
-         
+          <Route path="/login" element={<LoginForm/>} />
           {/* <Route path="/dashboard" component={Dashboard} /> */}
           {/* <Route path="/profile" component={UserProfile} /> */}
         </Routes>
